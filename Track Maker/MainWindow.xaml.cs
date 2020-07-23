a854f24a9ab82709a8f55db1f4977fc6dec4374d
Cosmo224 <mario64crashed@gmail.com>
2020-07-23 19:56:56 +0100

    disable if no project

--- Track Maker/MainWindow.xaml.cs
+++ Track Maker/MainWindow.xaml.cs
@@ -80,44 +80,6 @@ namespace Track_Maker
             Init_Phase2();
         }
 
-        /// <summary>
-        /// Determine the users' update check/telemetry consent status.
-        /// </summary>
-        public void Init_DetermineTelemetryConsentStatus()
-        {
-            // Ask the user.
-            if (Setting.TelemetryConsent == TelemetryConsent.NotDone)
-            {
-                if (MessageBox.Show("The Track Maker has auto-updating functionality.\n\n" +
-                    "To determine if an update is available, the Track Maker must connect to the Internet.\n\n" +
-                    "As a result of this a small amount of information is sent to the update server,\n" +
-                    "and it can be used to determine certain aspects of your activity - for example the date of each Track Maker start.\n" +
-                    "ABSOLUTELY NO personal information is sent to the update server and NO PERSONAL INFORMATION EVER WILL BE SENT.\n\n" +
-                    "This information is only a byproduct of the auto-update functionality and absolutely no information is\n" +
-                    "directly sent by the Track Maker.\n\n" +
-                    "Please note that if you choose not to check for updates, you will not be able to automatically update the Track Maker,\n" +
-                    "and you must do so manually.\n\n" +
-                    "Do you wish to check for updates at each start of the Track Maker?", "Check for Updates?", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
-                {
-                    EmeraldSettings.SetSetting("TelemetryConsent", "Yes");
-                }
-                else
-                {
-                    EmeraldSettings.SetSetting("TelemetryConsent", "No"); 
-                }
-            }
-            else
-            {
-                if (Setting.TelemetryConsent == TelemetryConsent.Yes)
-                {
-                    RunUpdater();
-                }
-                else
-                {
-                    return; 
-                }
-            }
-        }
 
         public void RunUpdater()
         {
@@ -150,6 +112,20 @@ namespace Track_Maker
 #elif PRISCILLA
             Title = "Track Maker \"Priscilla\" (version 1.5 alpha)";
 #endif
+
+            // DisableUI test 
+            if (CurrentProject == null)
+            {
+                FileMenu.IsEnabled = false;
+                ProjectMenu.IsEnabled = true;
+                EditMenu.IsEnabled = false;
+                StormMenu.IsEnabled = false;
+                ViewMenu.IsEnabled = false;
+                HelpMenu.IsEnabled = false; 
+                
+                
+            }
+
             TickTimer.Start();
             Logging.Log("Initialization completed.");
         }
--- Track Maker/Properties/AssemblyInfo.cs
+++ Track Maker/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.410.20205")]
-[assembly: AssemblyFileVersion("1.5.410.20205")]
+[assembly: AssemblyVersion("1.5.412.20205")]
+[assembly: AssemblyFileVersion("1.5.412.20205")]
