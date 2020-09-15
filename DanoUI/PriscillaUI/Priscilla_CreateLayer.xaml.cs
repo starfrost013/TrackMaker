be3478a4b1dc51dd1ccdefc5a12646b69a23b67a
Cosmo224 <mario64crashed@gmail.com>
2020-09-15 20:07:12 +0100

    Layer UI

--- DanoUI/PriscillaUI/Priscilla_CreateLayer.xaml.cs
+++ DanoUI/PriscillaUI/Priscilla_CreateLayer.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DanoUI.PriscillaUI
+namespace DanoUI
 {
     /// <summary>
     /// Interaction logic for CreateLayer.xaml
