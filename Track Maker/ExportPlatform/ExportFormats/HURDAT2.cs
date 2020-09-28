4fbbd37d71eb3319ff8d5b11f42edcb3cb924db7
Cosmo224 <mario64crashed@gmail.com>
2020-09-28 23:13:18 +0100

    fix the coordinates

--- Track Maker/ExportPlatform/ExportFormats/HURDAT2.cs
+++ Track Maker/ExportPlatform/ExportFormats/HURDAT2.cs
@@ -111,8 +111,10 @@ namespace Track_Maker
                             SW.Write($"{Abbv}, ");
 
                             Coordinate CD = Proj.SelectedBasin.FromNodePositionToCoordinate(No.Position);
-
-                            SW.Write($"{CD.Coordinates.X}.{CD.Directions[0].ToString()}, {CD.Coordinates.Y}{CD.Directions[0]}, ");
+                            
+                            // dumb fucking piece of shit hack because what the fuck is compatibility you fucking NOAA dumb fucks
+                            CD.Coordinates = new Point(CD.Coordinates.X / 100, CD.Coordinates.Y / 100); 
+                            SW.Write($"{CD.Coordinates.X}{CD.Directions[0].ToString()}, {CD.Coordinates.Y}{CD.Directions[0]}, ");
                             SW.WriteLine();
                         }
                     }
