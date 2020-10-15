3bae7d3ddb041dec58479576773dccf9fd425acc
Cosmo224 <mario64crashed@gmail.com>
2020-10-15 23:12:33 +0100

    some more

--- DanoUI/PriscillaUI/Priscilla_CreateLayer.xaml.cs
+++ DanoUI/PriscillaUI/Priscilla_CreateLayer.xaml.cs
@@ -23,10 +23,32 @@ namespace DanoUI
         
         public EventHandler<DanoEventArgs> BringToFrontHit { get; set; }
         public EventHandler<DanoEventArgs> BringToBackHit { get; set; }
+
+        /// <summary>
+        /// DanoEventArgs
+        /// 
+        /// DanoParameters:
+        /// 
+        /// 0 - bool - is bring to front clicked?
+        /// 
+        /// 1 - bool - is bring to back clicked?
+        /// 
+        /// 2 - string - the layer name to create
+        /// </summary>
         public EventHandler<DanoEventArgs> OKHit { get; set; }
         public CreateLayer()
         {
             InitializeComponent();
+        }
+
+        private void CreateLayer_OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            DanoEventArgs DEA = new DanoEventArgs();
+            DEA.DanoParameters.Add(CreateLayer_MoveToFront.IsChecked);
+            DEA.DanoParameters.Add(CreateLayer_MoveToBack.IsChecked);
+            DEA.DanoParameters.Add(CreateLayer_NameBox.Text);
+            // error checking goes here
+            OKHit(this, DEA);
         }
     }
 }
