a010502f6e4164c9b8d4c75115c5d3aa3b1776ea
starfrost <mario64crashed@gmail.com>
2020-11-08 00:58:17 +0000

    oof

--- Track Maker/DanoProject/Upgrade/ProjectUpgrade.cs
+++ Track Maker/DanoProject/Upgrade/ProjectUpgrade.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Track_Maker
 {
+    /// <summary>
+    /// Make non-static?
+    /// </summary>
     public static class ProjectUpgrader
     {
 
@@ -17,6 +22,22 @@ namespace Track_Maker
         public static bool V1ToV2Upgrade(string V1FileName)
         {
             // To complete  2020-11-08
+
+            XmlDocument XMLD = new XmlDocument();
+            XMLD.Load(V1FileName); 
+
+            // probably a terrible idea code
+            foreach (XmlNode XNode in XMLD)
+            {
+                if (XNode.Name == "FormatVersionMajor" || XNode.Name == "FormatVersionMinor")
+                {
+                    Error.Throw("Error", "Attempted to upgrade Version 2.x file; this is not a version 1.x format file!", ErrorSeverity.Error, 193);
+                    return true; 
+                }
+
+
+            }
+            // end probably a terrible idea code
         }
     }
 }
