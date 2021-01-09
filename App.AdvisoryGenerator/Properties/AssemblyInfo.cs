aa4c80782768331334f1d987319c25d3fb2ab2e3
starfrost <mario64crashed@gmail.com>
2021-01-09 23:34:54 +0000

    649b: more UI

--- App.AdvisoryGenerator/Properties/AssemblyInfo.cs
+++ App.AdvisoryGenerator/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.701.21009")]
-[assembly: AssemblyFileVersion("2.0.701.21009")]
+[assembly: AssemblyVersion("2.0.702.21009")]
+[assembly: AssemblyFileVersion("2.0.702.21009")]
