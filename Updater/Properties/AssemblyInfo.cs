2c9192a86d5c984f6713244bfa12b613aff3ca54
starfrost <mario64crashed@gmail.com>
2021-01-10 16:15:01 +0000

    final for 2.0; onto 2.1 we go!

--- Updater/Properties/AssemblyInfo.cs
+++ Updater/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("starfrost's Track Maker")]
-[assembly: AssemblyCopyright("Copyright © starfrost 2020")]
+[assembly: AssemblyCopyright("Copyright © 2019-2021 starfrost")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.378.21010")]
-[assembly: AssemblyFileVersion("2.0.378.21010")]
+[assembly: AssemblyVersion("2.0.650.21010")]
+[assembly: AssemblyFileVersion("2.0.650.21010")]
