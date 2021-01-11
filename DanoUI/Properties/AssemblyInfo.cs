46e0210c9604e70dd73bfe6e162cb2c388420aa0
starfrost <mario64crashed@gmail.com>
2021-01-11 20:03:57 +0000

    now compiles

--- DanoUI/Properties/AssemblyInfo.cs
+++ DanoUI/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.651.21011")]
-[assembly: AssemblyFileVersion("2.1.651.21011")]
+[assembly: AssemblyVersion("2.1.652.21011")]
+[assembly: AssemblyFileVersion("2.1.652.21011")]
